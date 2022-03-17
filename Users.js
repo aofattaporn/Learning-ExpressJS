1d1c81cb0a08fde3e083bf225caf20c9cb5957d3
Attaporn Peungsook <aofattapon321@gmail.com>
2022-03-17 22:47:30 +0700

    learn middleware and rest api

---
+++ Users.js
@@ -0,0 +1,20 @@
+const users = [
+   { 
+      id : 0,
+      name: "aof0",
+      email: "aof0@gmail.com"
+   },
+   { 
+      id : 1,
+      name: "aof2",
+      email: "aof1@gmail.com"
+   },
+   { 
+      id : 2,
+      name: "aof2",
+      email: "aof2@gmail.com"
+   }
+]
+
+
+module.exports = users;
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const express = require('express');
+const express = require('express'); 
+const path = require('path');
+const users = require('./Users');
+const logger = require('./middleware/logging');
 const app = express();
 
-app.get('/', (req, res) => {
-   res.end('hello');
+
+
+// Init Middleware 
+app.use(logger);
+
+
+// static folder 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/api/users', (req, res)=> {
+   res.json(users);
 })
+
+app.get('/api/user/:id', (req, res) => {
+   res.json(users.filter(user => user.id === parseInt(req.params.id)));
+})
+
+app.get('/file', (req, res) => {
+   res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/s', (req, res) => {
+   res.json({
+      data: "Hello world"
+   });
+
+   console.log("hello world");
+})
+
 
 
 const PORT = process.env.PORT || 3000; 
